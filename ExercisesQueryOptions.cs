9417081946ff87d7a0cda4f9f32e256de0d0ac63
APIVerve Team <hello@apiverve.com>
2024-09-21 01:10:59 -0500

    Bug Fixes

---
+++ ExercisesQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class ExercisesQueryOptions {
+public string muscle { get; set; }
+public string name { get; set; }
+public string equipment { get; set; }
+}
+
+}
